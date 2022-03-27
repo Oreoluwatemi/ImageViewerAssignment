70da88d7c609eef851852bb906d9f5bd5eee49db
Oreoluwa Lawal <temitayolawal35@gmail.com>
2022-03-26 22:50:50 -0400

    COMMIT

---
+++ ImageViewer/Model/Images.swift
@@ -0,0 +1,18 @@
+//
+//  Images.swift
+//  ImageViewer
+//
+//  Created by Oreoluwa Lawal on 2022-03-18.
+//
+
+import Foundation
+import UIKit
+class Images{
+    var title : String = "";
+    var img : UIImage;
+    
+    init(t : String, i : UIImage){
+        title = t
+        img = i
+    }
+}
